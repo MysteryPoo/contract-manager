f7269dc57dc3d970af7f00466b7d0ebd9641f76e
MysteryPoo <Matt.Thornton@knights.ucf.edu>
2020-10-24 11:51:12 -0400

    Transfer commit. Cannot run.

---
+++ routes/login.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+
+const admin = require('firebase-admin');
+const db = admin.firestore();
+
+const collections = require('../collections');
+
+router.get('/', async function(req, res, next) {
+    console.log("Requesting login");
+    const configRefQuery = await db.collection(collections['Settings']).doc('Config').get();
+    if (configRefQuery.empty) {
+        let error = "Fatal error: No server configuration found.";
+        console.log(error);
+        res.send(error);
+        return;
+    }
+    const config = configRefQuery.data();
+
+    res.render('login', {
+        title: `${config['Organization']} Login`,
+        banner: process.env.banner,
+        logo: process.env.logo,
+    });
+});
+
+module.exports = router;
