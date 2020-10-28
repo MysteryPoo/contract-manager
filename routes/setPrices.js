44fa74ea9283ac47726cf42dff9d9acfa6f246ee
MysteryPoo <Matt.Thornton@knights.ucf.edu>
2020-10-28 08:18:56 -0400

    Deprecated password field for admin actions

--- routes/setPrices.js
+++ routes/setPrices.js
@@ -10,6 +10,7 @@ const db = admin.firestore();
 router.get('/', async function(req, res, next) {
 
     if (!req.user || !req.user.isAdmin) {
+        req.flash('error', "Insufficient permissions for operation.");
         res.redirect('/');
         return;
     }
@@ -63,43 +64,30 @@ router.get('/', async function(req, res, next) {
 router.post('/', async function(req, res, next) {
 
     if (!req.user || !req.user.isAdmin) {
+        req.flash('error', "Insufficient permissions for operation.");
         res.redirect('/');
         return;
     }
 
-    const configRefQuery = await db.collection(collections['Settings']).doc('Config').get();
-    if (configRefQuery.empty) {
-        let error = "Fatal error: No server configuration found.";
-        console.log(error);
-        res.send(error);
-        return;
-    }
-    const config = configRefQuery.data();
+    const dateEntered = new Date().toISOString();
+    const docRef = db.collection(collections["Price-List"]).doc(dateEntered);
 
-    if (req.body['form-password'] === config['Password']) {
+    var priceList = {
+        'DateTime': dateEntered,
+        'Sell Weight': Number(req.body['form-sell-weight']),
+        'Buy Weight': Number(req.body['form-buy-weight'])
+    };
 
-        const dateEntered = new Date().toISOString();
-        const docRef = db.collection(collections["Price-List"]).doc(dateEntered);
-
-        var priceList = {
-            'DateTime': dateEntered,
-            'Sell Weight': Number(req.body['form-sell-weight']),
-            'Buy Weight': Number(req.body['form-buy-weight'])
-        };
-
-        for (let category in materials) {
-            for (let material of materials[category]) {
-                let materialNoSpace = material.replace(/ /g, "");
-                priceList[material] = Number(req.body[`form-${materialNoSpace}`]);
-            }
+    for (let category in materials) {
+        for (let material of materials[category]) {
+            let materialNoSpace = material.replace(/ /g, "");
+            priceList[material] = Number(req.body[`form-${materialNoSpace}`]);
         }
-
-        await docRef.set(priceList);
-
-        req.flash('success', "Updated prices");
-    } else {
-        req.flash('error', "BAD PASSWORD");
     }
+
+    await docRef.set(priceList);
+
+    req.flash('success', "Updated prices");
     res.redirect(req.baseUrl);
 });
 
