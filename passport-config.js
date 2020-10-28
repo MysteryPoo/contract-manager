9a74514bc364e565d6d731c793d1dc337563adb5
MysteryPoo <Matt.Thornton@knights.ucf.edu>
2020-10-28 08:17:10 -0400

    Updated passport config

--- passport-config.js
+++ passport-config.js
@@ -1,28 +1,28 @@
 const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcrypt');
 
-function initialize(passport, getUserByEmail, getUserById) {
-  const authenticateUser = async (email, password, done) => {
-    const user = getUserByEmail(email);
+function initialize(passport, getUserByCharacterName, getUserById) {
+  const authenticateUser = async (characterName, password, done) => {
+    const user = await getUserByCharacterName(characterName);
     if (user == null) {
-      return done(null, false, { message: 'No user with that email' });
+      return done(null, false, { message: 'Character does not exist.' });
     }
 
     try {
       if (await bcrypt.compare(password, user.password)) {
         return done(null, user);
       } else {
-        return done(null, false, { message: 'Password incorrect' });
+        return done(null, false, { message: 'Password incorrect.' });
       }
     } catch (e) {
       return done(e);
     }
   };
 
-  passport.use(new LocalStrategy({ usernameField: 'email' }, authenticateUser));
+  passport.use(new LocalStrategy({ usernameField: 'CharacterName' }, authenticateUser));
   passport.serializeUser((user, done) => done(null, user.id));
-  passport.deserializeUser((id, done) => {
-    return done(null, getUserById(id));
+  passport.deserializeUser(async (id, done) => {
+    return done(null, await getUserById(id));
   });
 }
 
